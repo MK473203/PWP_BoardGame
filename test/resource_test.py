0b194a98c0c39f28bdf54d179807513017eef2e7
krasl <55877859+krasl@users.noreply.github.com>
2024-02-28 20:34:49 +0200

    Create resource_test.py

---
+++ test/resource_test.py
@@ -0,0 +1,242 @@
+import json
+import os
+import pytest
+import tempfile
+import time
+from flask.testing import FlaskClient
+from jsonschema import validate
+from sqlalchemy.engine import Engine
+from sqlalchemy import event
+from werkzeug.datastructures import Headers
+
+from app import create_app, db
+from app.models import User, Game, GameType
+
+TEST_KEY = "verysafetestkey"
+
+class AuthHeaderClient(FlaskClient):
+
+    def open(self, *args, **kwargs):
+        api_key_headers = Headers({
+            'sensorhub-api-key': TEST_KEY
+        })
+        headers = kwargs.pop('headers', Headers())
+        headers.extend(api_key_headers)
+        kwargs['headers'] = headers
+        return super().open(*args, **kwargs)
+    
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
+
+
+@pytest.fixture
+def client():
+    db_fd, db_fname = tempfile.mkstemp()
+    config = {
+        "SQLALCHEMY_DATABASE_URI": "sqlite:///" + db_fname,
+        "TESTING": True
+    }
+    
+    app = create_app(config)
+    
+    with app.app_context():
+        db.create_all()
+        _populate_db()
+        
+    app.test_client_class = AuthHeaderClient
+    yield app.test_client()
+    
+    os.close(db_fd)
+    os.unlink(db_fname)
+
+def _populate_db():
+    for i in range(1, 4):
+        u = User(
+            name="testuser_{}".format(i),
+            password="test123{}"
+        )
+        db.session.add(u)
+     
+    db_key = ApiKey(
+        key=ApiKey.key_hash(TEST_KEY),
+        admin=True
+    )
+    db.session.add(db_key)        
+    db.session.commit()
+    
+def _get_user_json(number=1):
+    """
+    Creates a valid user JSON object to be used for PUT and POST tests.
+    """
+    
+    return {"name": "extra-user-{}".format(number), "password": "extra{}".format(number)}
+    
+def _check_namespace(client, response):
+    """
+    Checks that the "senhub" namespace is found from the response body, and
+    that its "name" attribute is a URL that can be accessed.
+    """
+    
+    ns_href = response["@namespaces"]["senhub"]["name"]
+    resp = client.get(ns_href)
+    assert resp.status_code == 200
+    
+def _check_control_get_method(ctrl, client, obj):
+    """
+    Checks a GET type control from a JSON object be it root document or an item
+    in a collection. Also checks that the URL of the control can be accessed.
+    """
+    
+    href = obj["@controls"][ctrl]["href"]
+    resp = client.get(href)
+    assert resp.status_code == 200
+    
+def _check_control_delete_method(ctrl, client, obj):
+    """
+    Checks a DELETE type control from a JSON object be it root document or an
+    item in a collection. Checks the contrl's method in addition to its "href".
+    Also checks that using the control results in the correct status code of 204.
+    """
+    
+    href = obj["@controls"][ctrl]["href"]
+    method = obj["@controls"][ctrl]["method"].lower()
+    assert method == "delete"
+    resp = client.delete(href)
+    assert resp.status_code == 204
+    
+def _check_control_put_method(ctrl, client, obj):
+    """
+    Checks a PUT type control from a JSON object be it root document or an item
+    in a collection. In addition to checking the "href" attribute, also checks
+    that method, encoding and schema can be found from the control. Also
+    validates a valid sensor against the schema of the control to ensure that
+    they match. Finally checks that using the control results in the correct
+    status code of 204.
+    """
+    
+    ctrl_obj = obj["@controls"][ctrl]
+    href = ctrl_obj["href"]
+    method = ctrl_obj["method"].lower()
+    encoding = ctrl_obj["encoding"].lower()
+    schema = ctrl_obj["schema"]
+    assert method == "put"
+    assert encoding == "json"
+    body = _get_sensor_json()
+    body["name"] = obj["name"]
+    validate(body, schema)
+    resp = client.put(href, json=body)
+    assert resp.status_code == 204
+    
+def _check_control_post_method(ctrl, client, obj):
+    """
+    Checks a POST type control from a JSON object be it root document or an item
+    in a collection. In addition to checking the "href" attribute, also checks
+    that method, encoding and schema can be found from the control. Also
+    validates a valid sensor against the schema of the control to ensure that
+    they match. Finally checks that using the control results in the correct
+    status code of 201.
+    """
+    
+    ctrl_obj = obj["@controls"][ctrl]
+    href = ctrl_obj["href"]
+    method = ctrl_obj["method"].lower()
+    encoding = ctrl_obj["encoding"].lower()
+    schema = ctrl_obj["schema"]
+    assert method == "post"
+    assert encoding == "json"
+    body = _get_sensor_json()
+    validate(body, schema)
+    resp = client.post(href, json=body)
+    assert resp.status_code == 201
+
+class TestUserCollection(object):
+    
+    RESOURCE_URL = "/api/users/"
+    VALID_USER_DATA = {
+        "name": "test-user-5",
+        "password": "testpassword"
+    }
+    INVALID_USER_DATA = {
+        "name": "test-user-y"
+    }
+
+    def test_get(self, client):
+        # assuming user is logged in as admin
+        resp = client.get(self.RESOURCE_URL)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        # check if response contains a list of users
+        assert isinstance(body, list)
+        assert len(body) > 0
+        # check if each user has 'id' and 'name' fields
+        for user in body:
+            assert 'id' in user
+            assert 'name' in user
+
+    def test_post(self, client):
+        # test with wrong content type
+        resp = client.post(self.RESOURCE_URL, data="notjson", headers={"Content-Type": "text"})
+        assert resp.status_code in (400, 415)
+        
+        # test with invalid user data
+        resp = client.post(self.RESOURCE_URL, json=self.INVALID_USER_DATA)
+        assert resp.status_code == 400
+        
+        # test with valid user data
+        resp = client.post(self.RESOURCE_URL, json=self.VALID_USER_DATA)
+        assert resp.status_code == 201
+        # check if the user is created
+        assert User.query.filter_by(name=self.VALID_USER_DATA["name"]).first() is not None
+
+   
+class TestUserItem(object):
+    
+    RESOURCE_URL = "/api/users/test-user-1/"
+    INVALID_URL = "/api/users/non-user-x/"
+    
+    def test_get(self, client):
+        resp = client.get(self.RESOURCE_URL)
+        assert resp.status_code == 200
+        body = json.loads(resp.data)
+        _check_namespace(client, body)
+        _check_control_get_method("id", client, body)
+        _check_control_get_method("name", client, body)
+        resp = client.get(self.INVALID_URL)
+        assert resp.status_code == 404
+
+    def test_put(self, client):
+        valid = _get_user_json()
+        
+        # test with wrong content type
+        resp = client.put(self.RESOURCE_URL, data="notjson", headers=Headers({"Content-Type": "text"}))
+        assert resp.status_code in (400, 415)
+        
+        resp = client.put(self.INVALID_URL, json=valid)
+        assert resp.status_code == 404
+        
+        # test with another users's name
+        valid["name"] = "test-user-2"
+        resp = client.put(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 409
+        
+        # test with valid
+        valid["name"] = "test-user-1"
+        resp = client.put(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 204
+        
+        # remove field for 400
+        valid.pop("id")
+        resp = client.put(self.RESOURCE_URL, json=valid)
+        assert resp.status_code == 400
+        
+    def test_delete(self, client):
+        resp = client.delete(self.RESOURCE_URL)
+        assert resp.status_code == 204
+        resp = client.delete(self.RESOURCE_URL)
+        assert resp.status_code == 404
+        resp = client.delete(self.INVALID_URL)
+        assert resp.status_code == 404
