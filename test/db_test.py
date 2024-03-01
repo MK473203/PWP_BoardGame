062a7473d080dd1533519f73ab573fc7d643dd46
krasl <55877859+krasl@users.noreply.github.com>
2024-03-01 21:25:38 +0200

    Update db_test.py

--- test/db_test.py
+++ test/db_test.py
@@ -8,12 +8,13 @@ from app import create_app, db
 from app.game_logic import apply_move
 from app.models import User, Game, GameType
 
-
+"""
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
     cursor = dbapi_connection.cursor()
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
+"""
 
 
 @pytest.fixture
